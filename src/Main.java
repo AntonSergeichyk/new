92c4ecf1afabaa734a7f1858c971220c01aec5ae
Илья Касперович <i.kasperovich@gigant.by>
2018-04-25 20:43:15 +0300

    sfdfdf

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-package PACKAGE_NAME;
-
 /**
  * Created by user on 25.04.2018.
  */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
